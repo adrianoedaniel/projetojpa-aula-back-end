5e0438b0dc37eebeea551f0ab84bfd1c77b07327
Adriano Daniel <adrianoedaniel@gmail.com>
2023-08-25 16:33:07 -0300

    App Back-end JPA

---
+++ src/test/java/com/projetojpa/Projetojpa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetojpa01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
